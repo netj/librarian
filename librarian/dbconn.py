c4217b1c147b792880d37975dbcc99c47754e87b
abhirast <arastogi@stanford.edu>
2015-02-27 15:51:46 -0800

    Added functions for creating projects. uploading files and downloading files

--- librarian/dbconn.py
+++ librarian/dbconn.py
@@ -10,29 +10,29 @@ Schema:
 'Engagements': ['id', 'name', 'date_started', 'owner', 'comments'], 
 
 'IncomingData': ['id', 'project', 'name', 'version', 'timestamp', 
-      'urls', 'checksums', 'metadata_url', 'comments', 'user', 'hostname']
+      'urls', 'checksums', 'metadata_url', 'comments', 'username', 'hostname']
 
 'OutgoingData': ['id', 'project', 'name', 'version', 'timestamp', 
-      'urls', 'checksums', 'metadata_url', 'comments', 'user', 'hostname']
+      'urls', 'checksums', 'metadata_url', 'comments', 'username', 'hostname']
 
 """
 
-import MySQLdb, datetime
+import MySQLdb, datetime, urllib
 Schema = {
 'Engagements': ['id', 'name', 'date_started', 'owner', 'comments'], 
 
 'IncomingData': ['id', 'project', 'name', 'version', 'timestamp', 
-      'urls', 'checksums', 'metadata_url', 'comments', 'user', 'hostname'],
+      'urls', 'checksums', 'metadata_url', 'comments', 'username', 'hostname'],
 
 'OutgoingData': ['id', 'project', 'name', 'version', 'timestamp', 
-      'urls', 'checksums', 'metadata_url', 'comments', 'user', 'hostname']
+      'urls', 'checksums', 'metadata_url', 'comments', 'username', 'hostname']
 }
 
 class DBConn:
   """Represents a live database conn with Librarian-specific operators."""
 
-  def __init__(self, username, password, host, port = 3306):
-    self.user = username
+  def __init__(self, user=None, password=None, host=None, port = 3306):
+    self.user = user
     self.pswd = password
     self.host = host
     self.port = int(port)
@@ -60,19 +60,26 @@ class DBConn:
 
     def datasetQueryFor(table):
         return '''
-        select ds.name, ds.version, ds.urls
+        select ds.name, ds.version, ds.timestamp
           from %s ds
           join Engagements on ds.project = Engagements.id
          where Engagements.name = %%s
         ''' % (table)
     try:
       for _ in xrange(c.execute(datasetQueryFor('IncomingData') +
-                                ' union ' + datasetQueryFor('OutgoingData'),
+                                ' union all ' + datasetQueryFor('OutgoingData'),
                                 (project, project))):
         yield c.fetchone()
     except Exception as e:
       raise Exception('Database not available', e)
     c.close()
+
+  def _getTableSignature(self, table):
+    ''' A private function to ease query generation during insertion '''
+    cols = Schema[table][1:]        # skip the autoincrement field
+    return '''insert into %s(%s) values (%s)''' % (table, ','.join(cols), 
+                        ','.join(['%s']*len(cols)))
+
     
   def createProject(self, project, comments=''):
     ''' Creates a new project in the database. This function should be called
@@ -83,10 +90,71 @@ class DBConn:
     c = self.db.cursor()
     date = datetime.date.today()
     owner = self.user
-    relation = 'Engagements(%s, %s, %s, %s)' % tuple(Schema['Engagements'][1:])
-    c.execute('insert into ' + relation + ' values (%s, %s, %s, %s)',
+    c.execute(self._getTableSignature('Engagements'),
                 (project, date, owner, comments))
     self.db.commit()
     c.close()
+      
+  def addInOrOut(self, project, name, version, timestamp, urls, checksums, 
+                    metadata_url, comments, username, hostname, incoming=True):
+    ''' Adds info about incoming or outgoing file(s) to librarian database. 
+        The incoming or outgoing files must belong to an existing project.
+        The timestamp should be a datetime object.
+    '''
     
-
+    if project not in self.projectLs():
+      raise Exception('Project does not exist!')
+    
+    # Get the project id
+    c = self.db.cursor()
+    c.execute('select id from Engagements where name=%s', (project,))
+    pid = c.fetchone()[0]
+    
+    # Add the record to the specified relation
+    relation = 'IncomingData' if incoming else 'OutgoingData'
+    record = (pid, name, version, timestamp, urls, checksums, metadata_url, 
+                comments, username, hostname)
+    c.execute(self._getTableSignature(relation), record)
+    self.db.commit()
+    c.close()
+    
+  def fetch(self, project, dataset, version, localpath):
+    c = self.db.cursor()
+    def datasetQueryFor(table):
+        return '''
+        select ds.urls 
+          from %s ds
+          join Engagements on ds.project = Engagements.id
+          where Engagements.name=%%s and ds.name=%%s and ds.version=%%s
+        ''' % (table)
+    try:
+      for _ in xrange(c.execute(datasetQueryFor('IncomingData') +
+                                ' union all ' + datasetQueryFor('OutgoingData'),
+                                (project, dataset, version, project, dataset, version))):
+        for url in c.fetchone()[0].split():
+            # TODO Maintain the directory structure
+            ind = url.rindex('/')
+            urllib.urlretrieve(url, filename= localpath+url[ind:])
+    except Exception:
+      raise Exception('Database not available')
+    c.close()
+    return localpath
+    
+if __name__=='__main__':
+    import json, os, datetime
+    cred = json.load(open(os.path.abspath(os.path.expanduser("~/.librarian"))))
+    cred = cred['credentials']['mysql']
+    conn = DBConn(**cred)
+    # list all projects
+    projects = [x for x in conn.projectLs()]
+    print projects
+    # list all files for the first project
+    files = [x for x in conn.ls(projects[0])]
+    print files
+    # testing addInOrOut
+    conn.addInOrOut(projects[0], 'test1', '0.0', datetime.datetime.now(), 'urls', 
+                        'checksums', 'meta_url', 'some comments', 'dummyuser', 'dummydomain', incoming=False)
+                        
+    # testing download
+    conn = DBConn(**cred)
+    conn.fetch('project1', 'test', '0.0', '/home/abhinav/delete')
