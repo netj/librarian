ae21e6cdcfca245e24f09d7d996e7b4f13c83e6c
abhirast <arastogi@stanford.edu>
2015-02-27 13:57:59 -0800

    Updated to test auto increment feature

--- librarian/debug.sql
+++ librarian/debug.sql
@@ -1,13 +1,24 @@
+/*
+Schema:
+'Engagements': ['id', 'name', 'date_started', 'owner', 'comments'], 
+
+'IncomingData': ['id', 'project', 'name', 'version', 'timestamp', 
+      'urls', 'checksums', 'metadata_url', 'comments', 'username', 'hostname']
+
+'OutgoingData': ['id', 'project', 'name', 'version', 'timestamp', 
+      'urls', 'checksums', 'metadata_url', 'comments', 'username', 'hostname']
+*/
+
 -- dummy values for testing the database
 
-insert into Engagements values (1, 'project1', '2014-05-02', 'abhinav', 'project one');
-insert into Engagements values (2, 'project2', '2014-06-01', 'abhinav', 'project two');
-insert into Engagements values (3, 'project3', '2014-08-05', 'abhinav', 'project three');
+insert into Engagements(name, date_started, owner, comments) values ('project1', '2014-05-02', 'abhinav', 'project one');
+insert into Engagements(name, date_started, owner, comments) values ('project2', '2014-06-01', 'abhinav', 'project two');
+insert into Engagements(name, date_started, owner, comments) values ('project3', '2014-08-05', 'abhinav', 'project three');
 
-insert into IncomingData values (1, 1, 'init', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'input 1', 'abhinav', 'stanford');
-insert into IncomingData values (2, 1, 'two', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'input 2', 'abhinav', 'stanford');
-insert into IncomingData values (3, 2, 'three', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'input 3', 'abhinav', 'stanford');
+insert into IncomingData(project, name, version, timestamp, urls, checksums, metadata_url, comments, username, hostname) values (1, 'init', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'input 1', 'abhinav', 'stanford');
+insert into IncomingData(project, name, version, timestamp, urls, checksums, metadata_url, comments, username, hostname) values (1, 'two', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'input 2', 'abhinav', 'stanford');
+insert into IncomingData(project, name, version, timestamp, urls, checksums, metadata_url, comments, username, hostname) values (2, 'three', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'input 3', 'abhinav', 'stanford');
 
-insert into OutgoingData values (1, 2, 'init', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'output 1', 'abhinav', 'stanford');
-insert into OutgoingData values (2, 2, 'two', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'output 2', 'abhinav', 'stanford');
-insert into OutgoingData values (3, 1, 'three', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'output 3', 'abhinav', 'stanford');
+insert into OutgoingData(project, name, version, timestamp, urls, checksums, metadata_url, comments, username, hostname) values (2, 'init', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'output 1', 'abhinav', 'stanford');
+insert into OutgoingData(project, name, version, timestamp, urls, checksums, metadata_url, comments, username, hostname) values (2, 'two', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'output 2', 'abhinav', 'stanford');
+insert into OutgoingData(project, name, version, timestamp, urls, checksums, metadata_url, comments, username, hostname) values (1, 'three', '0.01', '2014-05-03 12:12:12:1234', 'many urls', 'checksum', 'meta_url', 'output 3', 'abhinav', 'stanford');
